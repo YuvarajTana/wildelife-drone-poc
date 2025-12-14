36aba92fcd1f4a1535eb141a51fca9dae2576523
Yuvaraj Tana <yuvarajtana@gmail.com>
2025-12-14 11:11:27 +0530

    Added .gitignore changes

---
+++ backend/main.py
@@ -0,0 +1,33 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"message": "Hello, World!"} 
+
+class Item(BaseModel):
+    name: str
+    price: float
+    is_offer: bool = False
+
+@app.get("/items")
+def read_items():
+    return {"message": "Items!"}
+
+
+@app.post("/items")
+def create_item():
+    return {"message": "Item created!"}
+
+@app.put("/items/{item_id}")
+def update_item(item_id: int ):  
+    return {"message": "Item updated!"}
+
+@app.delete("/items/{item_id}")
+def delete_item(item_id: int):
+    return {"message": "Item deleted!"}
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int):
+    return {"message": "Item!", "item_id": item_id}
